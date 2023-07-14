b0de5c4e4d5aeb177e65b317137a9a282930ca30
thomas <tamighi@hotmail.com>
2023-07-14 11:42:50 +0200

    Add StarCanvas

--- src/components/StarCanvas/StarCanvas.tsx
+++ src/components/StarCanvas/StarCanvas.tsx
@@ -1,5 +1,47 @@
+import React from "react";
+import { PointMaterial, Points, Preload } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Suspense } from "react";
+import { random } from "maath";
+
+const Stars = () => {
+  const ref = React.useRef<any>(null);
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 }) as Float32Array;
+
+  useFrame((_, delta) => {
+    if (!ref.current) {
+      return null
+    }
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled>
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
+
 const StarCanvas = () => {
-  return <div>StarCanvas</div>;
+  return (
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+          <Preload all />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 };
 
 export default StarCanvas;
