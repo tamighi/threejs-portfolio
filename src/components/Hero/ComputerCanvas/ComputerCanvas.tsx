ca74d3560d51483dcd209b0ef93e4c6e79db1a8a
thomas <tamighi@hotmail.com>
2023-07-15 09:29:35 +0200

    Worked on ComputerCanvas

--- src/components/Hero/ComputerCanvas/ComputerCanvas.tsx
+++ src/components/Hero/ComputerCanvas/ComputerCanvas.tsx
@@ -1,12 +1,53 @@
 import React from "react";
 
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { Suspense } from "react";
 import { CanvasLoader } from "../..";
 
+const duration = 8000;
+
+const smoothStep = (x: number) => {
+  return x * x * (3 - 2 * x);
+};
+
 const Computers = () => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computerRef = React.useRef<any>(null);
+  const [direction, setDirection] = React.useState<"right" | "left">("left");
+  const [transitionProgress, setTransitionProgress] = React.useState(0);
+
+  useFrame(() => {
+    if (computerRef.current) {
+      const rotation = 0.002;
+      const speed = Math.abs(Math.abs(transitionProgress - 1) - 1);
+      const easing = smoothStep(speed);
+      const rotationSpeed = rotation * easing;
+      if (direction === "right") {
+        computerRef.current.rotation.y += rotationSpeed;
+      } else {
+        computerRef.current.rotation.y -= rotationSpeed;
+      }
+    }
+  });
+
+  React.useEffect(() => {
+    const transitionInterval = setInterval(() => {
+      setTransitionProgress((prevProgress) => prevProgress + 0.02);
+    }, duration / 100);
+    return () => clearInterval(transitionInterval);
+  }, []);
+
+  React.useEffect(() => {
+    const directionInterval = setInterval(() => {
+      setDirection((prevDirection) =>
+        prevDirection === "right" ? "left" : "right"
+      );
+      setTransitionProgress(0);
+    }, duration);
+    return () => clearInterval(directionInterval);
+  }, []);
+
   const [isMobile, setIsMobile] = React.useState(false);
 
   React.useEffect(() => {
@@ -35,7 +76,8 @@ const Computers = () => {
         object={computer.scene}
         scale={isMobile ? 0.65 : 0.8}
         position={[0, -0.5, -1.5]}
-        rotation={[-0.01, 0.1, -0.05]}
+        rotation={[-0.01, 0, -0.05]}
+        ref={computerRef}
       />
     </mesh>
   );
@@ -44,7 +86,6 @@ const Computers = () => {
 const ComputersCanvas = () => {
   return (
     <Canvas
-      frameloop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
@@ -52,6 +93,7 @@ const ComputersCanvas = () => {
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
+          enablePan={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
